ba9507140ff24318cfc113a3989c7d041a37c8ff
Nachman Boguslavski <nbgslv@gmail.com>
2022-11-13 14:45:53 +0200

    added ignore eslint on build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
