7dc261cba9330fcb718267c9d81204b0e3740f1b
Nachman Boguslavski <nbgslv@gmail.com>
2022-12-01 19:18:44 +0200

    fixed eslint errors

--- pages/api/order/payment.ts
+++ pages/api/order/payment.ts
@@ -9,14 +9,19 @@ export default async function handler(
   try {
     const { method } = req;
     if (method === 'POST') {
+      // eslint-disable-next-line no-console
       console.log({ method, body: req.body });
     } else if (method === 'GET') {
+      // eslint-disable-next-line no-console
       console.log({ method, query: req.query });
     } else if (method === 'PUT') {
+      // eslint-disable-next-line no-console
       console.log({ method, body: req.body });
     } else if (method === 'DELETE') {
+      // eslint-disable-next-line no-console
       console.log({ method, body: req.body });
     } else {
+      // eslint-disable-next-line no-console
       console.log({ method, body: req.body });
       res.status(405).json({
         name: 'METHOD_NOT_ALLOWED',
