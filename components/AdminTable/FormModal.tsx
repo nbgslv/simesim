82acffc7720dd9209184a5d611f5f262141f1b30
nbgslv <nbgslv@gmail.com>
2023-01-01 20:03:52 +0200

    fixed lint errors

--- components/AdminTable/FormModal.tsx
+++ components/AdminTable/FormModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Modal } from 'react-bootstrap';
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
 import styles from './FormModal.module.scss';
