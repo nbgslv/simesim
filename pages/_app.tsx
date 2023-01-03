4ce46678e704dc940fa311fa009fa71f72122b1d
nbgslv <nbgslv@gmail.com>
2023-01-03 10:22:31 +0200

    added microsoft clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,6 +46,18 @@ function MyApp({ Component, pageProps }: AppProps) {
             gtag('config', 'G-Y16F868LV0');`,
         }}
       />
+      <Script
+        id="micro-clarity"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `            
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "f8eqfigs56");`,
+        }}
+      />
 
       <SSRProvider>
         <GoogleReCaptchaProvider
