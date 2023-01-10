4a0305cd7552aa485ae6ca0c71bfff94632a1d5c
nbgslv <nbgslv@gmail.com>
2023-01-10 20:29:40 +0200

    changed clarity script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,15 +50,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         id="micro-clarity"
         type="text/javascript"
         dangerouslySetInnerHTML={{
-          __html: `            
+          __html: `
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "f8eqfigs56");`,
+            })(window, document, "clarity", "script", "fc3ydfrtug");`,
         }}
       />
-
       <SSRProvider>
         <GoogleReCaptchaProvider
           reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string}
