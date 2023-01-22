eca2c5614e70cd75cdb22d7df13110fdb5ffbb9c
nbgslv <nbgslv@gmail.com>
2023-01-22 22:51:44 +0200

    removed unnes get static props from blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
-import { format } from 'date-fns';
 import { Col, Container, Row, Spinner } from 'react-bootstrap';
 import { Post } from '@prisma/client';
 import { useInView } from 'react-intersection-observer';
 import MainLayout from '../../components/Layouts/MainLayout';
 import PostCard from '../../components/Blog/PostCard';
-import prisma from '../../lib/prisma';
 import styles from '../../styles/blog.module.scss';
 import useInfiniteScroll from '../../utils/api/pagination/useInfiniteScroll';
 
@@ -65,43 +63,4 @@ const Index = () => {
   );
 };
 
-export const getStaticProps = async () => {
-  const totalPosts = await prisma.post.count({
-    where: {
-      show: true,
-    },
-  });
-
-  const posts = await prisma.post.findMany({
-    take: POST_QUANTITY,
-    where: {
-      show: true,
-    },
-    select: {
-      id: true,
-      slug: true,
-      title: true,
-      description: true,
-      coverImage: true,
-      createdAt: true,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
-
-  const serializedPosts = posts.map((post) => ({
-    ...post,
-    createdAt: format(post.createdAt, 'dd/MM/yy kk:mm'),
-  }));
-  const cursor = posts[posts.length - 1]?.id;
-
-  return {
-    props: {
-      posts: serializedPosts,
-      cursor,
-      total: totalPosts,
-    },
-  };
-};
 export default Index;
