eeadc8d4cbdc05a467d49addaae01680b6bc3167
nbgslv <nbgslv@gmail.com>
2023-02-21 21:27:57 +0200

    Revert "ignored type error"
    This reverts commit 967ea6215ca96b4c2e76a50143072595c0f405d1.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -20,6 +20,5 @@ Sentry.init({
   // sessions when an error occurs.
   replaysOnErrorSampleRate: process.env.NODE_ENV === 'production' ? 1.0 : 0.1,
 
-  // @ts-ignore
   integrations: [new Sentry.Replay()],
 });
