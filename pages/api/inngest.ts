573de486156ddbd8150287114c4e704138d27739
nbgslv <nbgslv@gmail.com>
2023-03-08 20:01:57 +0200

    added countries sync function to inngest api

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -1,9 +1,10 @@
 import { serve } from 'inngest/next';
 import blogUpload from '../../utils/inngest/blog';
+import countriesSync from '../../utils/inngest/countries';
 
 export default serve(
   'simesim',
-  [blogUpload], // A list of functions to expose.  This can be empty to start.
+  [blogUpload, countriesSync], // A list of functions to expose.  This can be empty to start.
   {
     signingKey: process.env.INNGEST_SIGNING_KEY,
   }
