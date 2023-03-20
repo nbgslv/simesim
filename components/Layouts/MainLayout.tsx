265f6fd8c6adeb6c555f99344cdb4651ca12e5fe
nbgslv <nbgslv@gmail.com>
2023-03-20 14:15:19 +0200

    added client only component to prevent hydration errors and added mobile exit intent

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -5,6 +5,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { Settings } from '@prisma/client';
 import { useCookies } from 'react-cookie';
+import { isMobile } from 'react-device-detect';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import styles from './MainLayout.module.scss';
@@ -14,6 +15,8 @@ import { Action } from '../../lib/reducer/settingsReducer';
 import HeaderRow from '../Header/HeaderRow';
 import ExitIntent from '../ExitIntent/ExitIntent';
 import ExitIntentModal from '../ExitIntent/ExitIntentModal';
+import useMobileExitIntent from '../ExitIntent/MobileExitIntent';
+import ClientOnly from '../ClientOnly/ClientOnly';
 
 const MainLayout = ({
   title,
@@ -35,6 +38,16 @@ const MainLayout = ({
   // eslint-disable-next-line @typescript-eslint/naming-convention,@typescript-eslint/no-unused-vars
   const [cookies, _, removeCookie] = useCookies(['exitModalSeen']);
   const { dispatch } = useSettingsStore() as Context<Action>;
+  useMobileExitIntent({
+    callback: () => {
+      if (
+        (!cookies.exitModalSeen || cookies.exitModalSeen !== 'true') &&
+        isMobile
+      ) {
+        setShowPopup(true);
+      }
+    },
+  });
   const router = useRouter();
 
   const handleScroll = useCallback(() => {
@@ -48,6 +61,7 @@ const MainLayout = ({
   useEffect(() => {
     if (
       (!cookies.exitModalSeen || cookies.exitModalSeen !== 'true') &&
+      !isMobile &&
       showExitIntent
     ) {
       const removeExitIntent = ExitIntent({
@@ -106,31 +120,33 @@ const MainLayout = ({
         )}
         {metaCanonical && <link rel="canonical" href={metaCanonical} />}
       </Head>
-      <ExitIntentModal hide={() => setShowPopup(false)} show={showPopup} />
-      <HeaderRow />
-      <CustomNavbar />
-      <Header hideJumbotron={hideJumbotron} />
-      <main>{children}</main>
-      <AnimatePresence>
-        {showOrderButton && router.pathname === '/' && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 1 }}
-          >
-            <Fab
-              href="/#bundles-section"
-              color="primary"
-              aria-label="order"
-              className={styles.fabOrder}
+      <ClientOnly>
+        <ExitIntentModal hide={() => setShowPopup(false)} show={showPopup} />
+        <HeaderRow />
+        <CustomNavbar />
+        <Header hideJumbotron={hideJumbotron} />
+        <main>{children}</main>
+        <AnimatePresence>
+          {showOrderButton && router.pathname === '/' && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
             >
-              לחבילות
-            </Fab>
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <Footer />
+              <Fab
+                href="/#bundles-section"
+                color="primary"
+                aria-label="order"
+                className={styles.fabOrder}
+              >
+                לחבילות
+              </Fab>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <Footer />
+      </ClientOnly>
     </>
   );
 };
