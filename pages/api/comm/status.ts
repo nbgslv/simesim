b7937c3e5888896c490b6c3d2d669b0b681ddd99
nbgslv <nbgslv@gmail.com>
2023-04-07 21:12:42 +0300

    added initial log for webhooks

---
+++ pages/api/comm/status.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ApiResponse } from '../../../lib/types/api';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<null>>
+) {
+  try {
+    const { method } = req;
+    if (method === 'POST') {
+      // eslint-disable-next-line no-console
+      console.log({ query: req.query, body: req.body });
+    } else {
+      res.status(405).json({
+        name: 'METHOD_NOT_ALLOWED',
+        success: false,
+        message: 'Method not allowed',
+      });
+    }
+  } catch (error: unknown) {
+    console.error(error);
+    res.status(500).json({
+      name: 'CONTACT_CREATION_ERR',
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
