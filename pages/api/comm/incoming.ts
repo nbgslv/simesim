c9f2ba0aca04600a3001aaea4632a37bfbb0e122
nbgslv <nbgslv@gmail.com>
2023-04-07 21:57:36 +0300

    fixed response way

--- pages/api/comm/incoming.ts
+++ pages/api/comm/incoming.ts
@@ -1,16 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ApiResponse } from '../../../lib/types/api';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ApiResponse<null>>
+  res: NextApiResponse
 ) {
   try {
     const { method } = req;
     if (method === 'POST') {
       // eslint-disable-next-line no-console
       console.log({ req });
-      res.status(200);
+      res
+        .setHeader('Content-Type', 'text/xml')
+        .status(200)
+        .send('<Response></Response>');
     } else {
       res.status(405).json({
         name: 'METHOD_NOT_ALLOWED',
