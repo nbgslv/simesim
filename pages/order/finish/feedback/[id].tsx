f4cdd9c2a31a4d70062ea5aa256cbef591b928d5
nbgslv <nbgslv@gmail.com>
2023-04-08 22:13:33 +0300

    added embedded feedback

---
+++ pages/order/finish/feedback/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Id = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <div>{id}</div>;
+};
+
+export default Id;
